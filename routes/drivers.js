4833febd0e4a094361a753dddb9266023dea486a
Ian Behrmann <ianjbehrmann@gmail.com>
2015-09-29 11:47:03 -0600

    Able to add drivers

--- routes/drivers.js
+++ routes/drivers.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var db = require('monk')('localhost/car-rental')
+var Drivers = db.get('drivers')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function (req, res, next) {
+  Drivers.find({}, function (err, data){  
+    res.render('drivers/index', { title: "List of Drivers",
+                                  allDrivers: data
+                                });
+  })
 });
+
+router.get('/new', function (req, res, next) {
+  res.render('drivers/new', { title: "Add a Driver"})
+})
+
+router.post('/new', function (req, res, next) {
+  Drivers.insert({  firstname: req.body.firstname,
+                    lastname: req.body.lastname,
+                    age: req.body.driverage,
+                    city: req.body.city,
+                    state: req.body.state
+                  })
+  res.redirect('/drivers')
+})
+
+router.get('/:id', function (req, res, next){
+  Drivers.findOne({_id: req.params.id}, function (err, data){
+    res.render('drivers/show', { theDriver: data })
+  })
+})
+
+router.get('/:id/edit', function (req, res, next) {
+  Drivers.findOne({_id: req.params.id}, function (err, data){
+    res.render('drivers/edit', { theDriver: data })
+  })
+})
 
 module.exports = router;
